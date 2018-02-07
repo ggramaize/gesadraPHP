20897ded301c18ab105812bea8ad9de5928ba346
ggramaize <geoffroy+dev@gramaize.eu>
2018-02-07 13:37:38 +0100

    Correction lisibilité impression liste membres reseau

--- impression_liste_membres_reseau.php
+++ impression_liste_membres_reseau.php
@@ -9,51 +9,52 @@ catch(Exception $e)
     die('Erreur : '.$e->getMessage());
 } ?>
 <html>
-<head>
-	<title>Impression de la liste des membres réseau</title>
-	<meta charset="UTF-8">
+	<head>
+		<title>Impression de la liste des membres réseau</title>
+		<meta charset="UTF-8" />
 
-	<style type="text/css">
-	.variable{
-			font-family: Courier;
-			font-type: monospace;
-		}
-	</style>
-</head>
-<script type="text/javascript">
-function edition()
-    {
-    options = "Width=700,Height=700" ;
-    window.open( "impression_liste_membres_reseau.php", "impression_liste_membres_reseau", options ) ;
-    }
-</script>
-<body>
-<?php $sql = "SELECT id, indicatif, alias_tactique, operateur, etat, commentaire FROM membres_reseau";
+		<style type="text/css">
+		.variable{
+				font-family: Courier;
+				font-type: monospace;
+			}
+		</style>
+		<script type="text/javascript">
+		function edition()
+		    {
+		    options = "Width=700,Height=700" ;
+		    window.open( "impression_liste_membres_reseau.php", "impression_liste_membres_reseau", options ) ;
+		    }
+		</script>
+	</head>
+	<body>
+<?php
+
+$sql = "SELECT id, indicatif, alias_tactique, operateur, etat, commentaire FROM membres_reseau";
 if ($res = $bdd->query($sql)) {
 
-   /* Récupère le nombre de lignes qui correspond à la requête SELECT */
-   if ($res->fetchColumn() > 0) {
+	/* Récupère le nombre de lignes qui correspond à la requête SELECT */
+	if ($res->fetchColumn() > 0) {
 
-      /* Effectue la vraie requête SELECT et travaille sur le résultat */
-      $sql = "SELECT id, indicatif, alias_tactique, operateur, etat, commentaire FROM membres_reseau";
-      foreach ($bdd->query($sql) as $row) {
-      print '
-      	<b>Indicatif : </b><span class="variable">'.strtoupper($row['indicatif']).'</span>
-      	</br>
-      	<b>Alias Tactique : </b><span class="variable">'.$row['alias_tactique'].'</span>
-      	</br>
-      	<b>Operateur : </b><span class="variable">'.$row['operateur'].'</span>
-      	</br>
-      	<b>Etat : </b><span class="variable">'.$row['etat'].'</span>
-      	</br>
-      	<b>Commentaire : </b><span class="variable">'.$row['commentaire'].'</span>
-      	</br></br>******************************</br></br>
-            ';
-      }
-   }} ?>
+		/* Effectue la vraie requête SELECT et travaille sur le résultat */
+		$sql = "SELECT id, indicatif, alias_tactique, operateur, etat, commentaire FROM membres_reseau";
+		foreach ($bdd->query($sql) as $row) {
+		?>
+		<b>Indicatif : </b><span class="variable"><?php echo( strtoupper($row['indicatif'])); ?></span><br />
+		<b>Alias Tactique : </b><span class="variable"><?php echo( $row['alias_tactique']); ?></span><br />
+		<b>Operateur : </b><span class="variable"><?php echo( $row['operateur']); ?></span><br />
+		<b>Etat : </b><span class="variable"><?php echo( $row['etat']); ?></span><br />
+		<b>Commentaire : </b><span class="variable"><?php echo( $row['commentaire']); ?></span><br />
+		<br />
+		******************************<br />
+		<br />
+<?php
+		}
+	}
+} ?>
 
-<script type="text/javascript">
-		window.print() ;
-	</script>
-</body>
+		<script type="text/javascript">
+			window.print() ;
+		</script>
+	</body>
 </html>
