27900268d148cccd894535921153249150ebe7d5
ggramaize <geoffroy+dev@gramaize.eu>
2018-02-08 06:47:12 +0100

    Nettoyage style embarqué nouveau membre + mutu CSS

--- nouveau-membre.php
+++ nouveau-membre.php
@@ -20,15 +20,15 @@ $tel_fixe_pro = "";
 $tel_port_pro = "";
 $adresse_mail_pro = "";
 
-$couleur_nom = "white";
-$couleur_prenom = "white";
-$couleur_indicatif = "white";
-$couleur_tel_fixe_perso = "white";
-$couleur_tel_port_perso = "white";
-$couleur_adresse_mail_perso = "white";
-$couleur_tel_fixe_pro = "white";
-$couleur_tel_port_pro = "white";
-$couleur_adresse_mail_pro = "white";
+$couleur_nom = "";
+$couleur_prenom = "";
+$couleur_indicatif = "";
+$couleur_tel_fixe_perso = "";
+$couleur_tel_port_perso = "";
+$couleur_adresse_mail_perso = "";
+$couleur_tel_fixe_pro = "";
+$couleur_tel_port_pro = "";
+$couleur_adresse_mail_pro = "";
 
 $placeholder_nom = "Nom";
 $placeholder_prenom = "Prénom";
@@ -44,63 +44,63 @@ if (isset($_POST['valider'])) {
 	
 	if (empty($_POST['nom'])) {
 		$placeholder_nom = "Merci de remplir ce champs";
-		$couleur_nom = "bisque";
+		$couleur_nom = 'class="fielderror"';
 	}else{
 		$nom = $_POST['nom'];
 	}
 
 	if (empty($_POST['prenom'])) {
 		$placeholder_prenom = "Merci de remplir ce champs";
-		$couleur_prenom = "bisque";
+		$couleur_prenom = 'class="fielderror"';
 	}else{
 		$prenom = $_POST['prenom'];
 	}
 
 	if (empty($_POST['indicatif'])) {
 		$placeholder_indicatif = "Merci de remplir ce champs";
-		$couleur_indicatif = "bisque";
+		$couleur_indicatif = 'class="fielderror"';
 	}else{
 		$indicatif = $_POST['indicatif'];
 	}
 
 	if (empty($_POST['fixe_perso'])) {
 		$placeholder_tel_fixe_perso = "Merci de remplir ce champs";
-		$couleur_tel_fixe_perso = "bisque";
+		$couleur_tel_fixe_perso = 'class="fielderror"';
 	}else{
 		$tel_fixe_perso = $_POST['fixe_perso'];
 	}
 
 	if (empty($_POST['port_perso'])) {
 		$placeholder_tel_port_perso = "Merci de remplir ce champs";
-		$couleur_tel_port_perso = "bisque";
+		$couleur_tel_port_perso = 'class="fielderror"';
 	}else{
 		$tel_port_perso = $_POST['port_perso'];
 	}
 
 	if (empty($_POST['mail_perso'])) {
 		$placeholder_adresse_mail_perso = "Merci de remplir ce champs";
-		$couleur_adresse_mail_perso = "bisque";
+		$couleur_adresse_mail_perso = 'class="fielderror"';
 	}else{
 		$adresse_mail_perso = $_POST['mail_perso'];
 	}
 
 	if (empty($_POST['fixe_pro'])) {
 		$placeholder_tel_fixe_pro = "Merci de remplir ce champs";
-		$couleur_tel_fixe_pro = "bisque";
+		$couleur_tel_fixe_pro = 'class="fielderror"';
 	}else{
 		$tel_fixe_pro = $_POST['fixe_pro'];
 	}
 
 	if (empty($_POST['port_pro'])) {
 		$placeholder_tel_port_pro = "Merci de remplir ce champs";
-		$couleur_tel_port_pro = "bisque";
+		$couleur_tel_port_pro = 'class="fielderror"';
 	}else{
 		$tel_port_pro = $_POST['port_pro'];
 	}
 
 	if (empty($_POST['mail_pro'])) {
 		$placeholder_adresse_mail_pro = "Merci de remplir ce champs";
-		$couleur_adresse_mail_pro = "bisque";
+		$couleur_adresse_mail_pro = 'class="fielderror"';
 	}else{
 		$adresse_mail_pro = $_POST['mail_pro'];
 	}
@@ -150,81 +150,76 @@ if (isset($_POST['valider'])) {
 	<head>
 		<title>Ajouter un nouveau membre</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="assets/style.css">
 	</head>
 	<body>
 		<form method="POST">
-			<table width="100%">
-				<tr width="100%">
-					<td colspan="2" style="background-color: orange; color: blue;">
-						<div align="center">
-							<span style="background: orange; font-size : 18px; color: blue;"><b>Ajouter un nouveau membre</b></span>
-							<br />
-						</div>
+			<table class="formfield">
+				<tr>
+					<td colspan="2" class="titre">Ajouter un nouveau membre</td>
+				</tr>
+				<tr>
+					<td>Nom :</td>
+					<td>
+						<input type="text" <?php echo $couleur_nom; ?> name="nom" id="nom" value="<?php echo $nom; ?>" placeholder="<?php echo $placeholder_nom; ?>">
 					</td>
 				</tr>
-				<tr width="100%">
-					<td width="30%">Nom :</td>
-					<td width="70%">
-						<input type="text" <?php echo 'style="width : 200px; background-color :  '.$couleur_nom.' ;"';?> name="nom" id="nom" value='<?php echo $nom; ?>' placeholder='<?php echo $placeholder_nom; ?>'>
+				<tr>
+					<td>Prénom :</td>
+					<td>
+						<input type="text" <?php echo $couleur_prenom; ?> name="prenom" id="prenom" value="<?php echo $prenom; ?>" placeholder="<?php echo $placeholder_prenom; ?>">
 					</td>
 				</tr>
-				<tr width="100%">
-					<td width="30%">Prénom :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_prenom; ?>;' name="prenom" id="prenom" value='<?php echo $prenom; ?>' placeholder ='<?php echo $placeholder_prenom; ?>'>
-					</td>
-				</tr>
-				<tr width="100%">
-					<td width="30%">Indicatif :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_indicatif; ?>;' name="indicatif" id="indicatif" value='<?php echo $indicatif; ?>' placeholder='<?php echo $placeholder_indicatif; ?>'>
+				<tr>
+					<td>Indicatif :</td>
+					<td>
+						<input type="text" <?php echo $couleur_indicatif; ?> name="indicatif" id="indicatif" value="<?php echo $indicatif; ?>" placeholder="<?php echo $placeholder_indicatif; ?>">
 					</td>
 				</tr>
 
-				<tr width="100%">
-					<td width="30%">Téléphone fixe personnel :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_tel_fixe_perso; ?>;' name="fixe_perso" id="fixe_perso" value='<?php echo $tel_fixe_perso; ?>' placeholder='<?php echo $placeholder_tel_fixe_perso; ?>'>
+				<tr>
+					<td>Téléphone fixe personnel :</td>
+					<td>
+						<input type="text" <?php echo $couleur_tel_fixe_perso; ?> name="fixe_perso" id="fixe_perso" value="<?php echo $tel_fixe_perso; ?>" placeholder="<?php echo $placeholder_tel_fixe_perso; ?>">
 					</td>
 				</tr>
-				<tr width="100%">
-					<td width="30%">Téléphone portable personnel :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_tel_port_perso; ?>;' name="port_perso" id="port_perso" value='<?php echo $tel_port_perso; ?>' placeholder='<?php echo $placeholder_tel_port_perso; ?>'>
+				<tr>
+					<td>Téléphone portable personnel :</td>
+					<td>
+						<input type="text" <?php echo $couleur_tel_port_perso; ?> name="port_perso" id="port_perso" value="<?php echo $tel_port_perso; ?>" placeholder="<?php echo $placeholder_tel_port_perso; ?>">
 					</td>
 				</tr>
-				<tr width="100%">
-					<td width="30%">Adresse e-mail personnelle :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_adresse_mail_perso; ?>;' name="mail_perso" id="mail_perso" value='<?php echo $adresse_mail_perso; ?>' placeholder='<?php echo $placeholder_adresse_mail_perso; ?>'>
-					</td>
-				</tr>
-
-				<tr width="100%">
-					<td width="30%">Téléphone fixe professionel :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_tel_fixe_pro; ?>;' name="fixe_pro" id="fixe_pro" value='<?php echo $tel_fixe_pro; ?>' placeholder='<?php echo $placeholder_tel_fixe_pro; ?>'>
+				<tr>
+					<td>Adresse e-mail personnelle :</td>
+					<td>
+						<input type="text" <?php echo $couleur_adresse_mail_perso; ?> name="mail_perso" id="mail_perso" value="<?php echo $adresse_mail_perso; ?>" placeholder="<?php echo $placeholder_adresse_mail_perso; ?>">
 					</td>
 				</tr>
 
-				<tr width="100%">
-					<td width="30%">Téléphone portable professionel :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_tel_port_pro; ?>;' name="port_pro" id="port_pro" value='<?php echo $tel_port_pro; ?>' placeholder='<?php echo $placeholder_tel_port_pro; ?>'>
+				<tr>
+					<td>Téléphone fixe professionel :</td>
+					<td>
+						<input type="text" <?php echo $couleur_tel_fixe_pro; ?> name="fixe_pro" id="fixe_pro" value="<?php echo $tel_fixe_pro; ?>" placeholder="<?php echo $placeholder_tel_fixe_pro; ?>">
 					</td>
 				</tr>
 
-				<tr width="100%">
-					<td width="30%">Adresse e-mail professionelle :</td>
-					<td width="70%">
-						<input type="text" style='width : 200px; background-color : <?php echo $couleur_adresse_mail_pro; ?>;' name="mail_pro" id="mail_pro" value='<?php echo $adresse_mail_pro; ?>' placeholder='<?php echo $placeholder_adresse_mail_pro; ?>'>
+				<tr>
+					<td>Téléphone portable professionel :</td>
+					<td>
+						<input type="text" <?php echo $couleur_tel_port_pro; ?> name="port_pro" id="port_pro" value="<?php echo $tel_port_pro; ?>" placeholder="<?php echo $placeholder_tel_port_pro; ?>">
 					</td>
 				</tr>
 
-				<tr width="100%">
-					<td colspan="2">
-						<div align="center">
-							<input type="submit" style="margin-top : 10px;" id="valider" name="valider" value="Valider">
+				<tr>
+					<td>Adresse e-mail professionelle :</td>
+					<td>
+						<input type="text" <?php echo $couleur_adresse_mail_pro; ?> name="mail_pro" id="mail_pro" value="<?php echo $adresse_mail_pro; ?>" placeholder="<?php echo $placeholder_adresse_mail_pro; ?>">
+					</td>
+				</tr>
+
+				<tr>
+					<td colspan="2" class="td_submit">
+							<input type="submit" id="valider" name="valider" value="Valider">
 					</td>
 				</tr>
 			</table>
