b872be9998e6e6831ade7fc7eed4e1404891e685
ggramaize <geoffroy+dev@gramaize.eu>
2018-02-08 07:23:47 +0100

    Correction lisibilité édition membres

--- edition-membres.php
+++ edition-membres.php
@@ -19,51 +19,48 @@ for( $i=1; $i<51; ++$i) {
 ?>
 
 <html>
-<head>
+	<head>
 		<title>Edition des membres</title>
 		<meta charset="UTF-8">
-<style type="text/css">
-.table1
-{
-    border-collapse: collapse;
-}
-.td1, .th1
-{
-    border: 1px solid black;
-}
-</style>
+		<link rel="stylesheet" type="text/css" href="assets/style.css" >
 	</head>
-<body>
-	
-		
+	<body>
 		<table width="202%">
-		<tr width="100%"><td style="background-color: orange; color:blue;" width="100%">
-			<div style="margin-left: 200px;"><span style="font-size : 18px;"><b>Edition de la liste des membres</b></span> <span style="font-size : 14px;">(Edition : un membre à la fois)</span></div>
-</td></tr>
-<tr><td>
-	<form method="POST" action="nouveau-membre.php">
-	<div style="margin-left : 20px; margin-top : 5px;"> <input type="submit" id="nouveau_membre" name="nouveau_membre" value="Ajouter un membre"></div>
-</form></td></tr>
-	</table>
+			<tr width="100%">
+				<td style="background-color: orange; color:blue;" width="100%">
+					<div style="margin-left: 200px;">
+						<span style="font-size : 18px;"><b>Edition de la liste des membres</b></span>
+						<span style="font-size : 14px;">(Edition : un membre à la fois)</span>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<form method="POST" action="nouveau-membre.php">
+						<div style="margin-left : 20px; margin-top : 5px;">
+							<input type="submit" id="nouveau_membre" name="nouveau_membre" value="Ajouter un membre">
+						</div>
+					</form>
+				</td>
+			</tr>
+		</table>
 
-	<div align="center">
-		<table class="table1" width="180%">
-			<form method="POST">
-      <thead>
-      <tr>
-      <th class="th1">ID</th>
-      <th class="th1">NOM</th>
-      <th class="th1">Prénom</th>
-      <th class="th1">Indicatif</th>
-      <th class="th1">Tel fixe Perso</th>
-      <th class="th1">Tel Port Perso</th>
-      <th class="th1">Mail Perso</th>
-      <th class="th1">Tel fixe Pro</th>
-      <th class="th1">Tel Port Pro</th>
-      <th class="th1">Mail Pro</th>
-      <th class="th1">XXXX</th>
-      </tr>
-      </thead>
+		<div align="center">
+			<table class="table1" width="180%">
+				<form method="POST">
+					<tr>
+						<th class="th1">ID</th>
+						<th class="th1">NOM</th>
+						<th class="th1">Prénom</th>
+						<th class="th1">Indicatif</th>
+						<th class="th1">Tel fixe Perso</th>
+						<th class="th1">Tel Port Perso</th>
+						<th class="th1">Mail Perso</th>
+						<th class="th1">Tel fixe Pro</th>
+						<th class="th1">Tel Port Pro</th>
+						<th class="th1">Mail Pro</th>
+						<th class="th1">XXXX</th>
+					</tr>
 <?php $sql = "SELECT id, nom_membre, prenom_membre, indicatif_membre, tel_fixe_perso, tel_portable_perso, mail_perso, tel_fixe_pro, tel_portable_pro, mail_pro FROM membres";
 if ($res = $bdd->query($sql)) {
 
@@ -73,41 +70,46 @@ if ($res = $bdd->query($sql)) {
       /* Effectue la vraie requête SELECT et travaille sur le résultat */
       $sql = "SELECT id, nom_membre, prenom_membre, indicatif_membre, tel_fixe_perso, tel_portable_perso, mail_perso, tel_fixe_pro, tel_portable_pro, mail_pro FROM membres";
       foreach ($bdd->query($sql) as $row) {
-      	
-      print '
-      	<tr>
-      	<td class="td1">
-      	'.$row['id'].'
-      	</td><td class="td1">
-      	<input type="text" id="nom_'.$row['id'].'" name="nom_'.$row['id'].'" value="'.strtoupper($row['nom_membre']).'">
-      	</td><td class="td1">
-      	<input type="text" id="prenom_'.$row['id'].'" name="prenom_'.$row['id'].'" value="'.ucfirst($row['prenom_membre']).'">	
-      	</td><td class="td1">
-      	<input type="text" id="indicatif_'.$row['id'].'" name="indicatif_'.$row['id'].'" value="'.$row['indicatif_membre'].'">
-      	</td><td class="td1">
-      	<input type="text" id="fixe_perso_'.$row['id'].'" name="fixe_perso_'.$row['id'].'" value="'.$row['tel_fixe_perso'].'">
-      	</td><td class="td1">
-      	<input type="text" id="port_perso_'.$row['id'].'" name="port_perso_'.$row['id'].'" value="'.$row['tel_portable_perso'].'">
-      	</td><td class="td1">
-      	<input type="text" id="mail_perso_'.$row['id'].'" name="mail_perso_'.$row['id'].'" value="'.$row['mail_perso'].'">
-      	</td><td class="td1">
-      	<input type="text" id="fixe_pro_'.$row['id'].'" name="fixe_pro_'.$row['id'].'" value="'.$row['tel_fixe_pro'].'">
-      	</td><td class="td1">
-      	<input type="text" id="port_pro_'.$row['id'].'" name="port_pro_'.$row['id'].'" value="'.$row['tel_portable_pro'].'">
-      	</td><td class="td1">
-      	<input type="text" id="mail_pro_'.$row['id'].'" name="mail_pro_'.$row['id'].'" value="'.$row['mail_pro'].'">
-      	</td><td class="td1">
-      	<input type="submit" id="'.$row['id'].'" name="'.$row['id'].'" value="Valider">
-      	</td>
-      	</tr>
-            ';
-      }
-   }} ?>
-</form>
-   </table>
-</div>
- 
-
-	
-</body>
+?>	
+<tr>
+	<td class="td1"><?php echo( $row['id']); ?></td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'nom_'.$row['id']); ?>" name="<?php echo( 'nom_'.$row['id']); ?>" value="<?php echo( strtoupper($row['nom_membre'])); ?>">
+	</td>
+	<td class="td1">
+	<input type="text" id="<?php echo( 'prenom_'.$row['id']); ?>" name="<?php echo( 'prenom_'.$row['id']); ?>" value="<?php echo( ucfirst($row['prenom_membre'])); ?>">	
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'indicatif_'.$row['id']); ?>" name="<?php echo( 'indicatif_'.$row['id']); ?>" value="<?php echo( $row['indicatif_membre']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'fixe_perso_'.$row['id']); ?>" name="<?php echo( 'fixe_perso_'.$row['id']); ?>" value="<?php echo( $row['tel_fixe_perso']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'port_perso_'.$row['id']); ?>" name="<?php echo( 'port_perso_'.$row['id']); ?>" value="<?php echo( $row['tel_portable_perso']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'mail_perso_'.$row['id']); ?>" name="<?php echo( 'mail_perso_'.$row['id']); ?>" value="<?php echo($row['mail_perso']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'fixe_pro_'.$row['id']); ?>" name="<?php echo( 'fixe_pro_'.$row['id']); ?>" value="<?php echo( $row['tel_fixe_pro']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'port_pro_'.$row['id']); ?>" name="<?php echo( 'port_pro_'.$row['id']); ?>" value="<?php echo( $row['tel_portable_pro']); ?>">
+	</td>
+	<td class="td1">
+		<input type="text" id="<?php echo( 'mail_pro_'.$row['id']); ?>" name="<?php echo( 'mail_pro_'.$row['id']); ?>" value="<?php echo( $row['mail_pro']); ?>">
+	</td>
+	<td class="td1">
+		<input type="submit" id="<?php echo( $row['id']); ?>" name="<?php echo( $row['id']); ?>" value="Valider">
+	</td>
+</tr>
+<?php
+		}
+	}
+} ?>
+				</form>
+			</table>
+		</div>
+	</body>
 </html>
