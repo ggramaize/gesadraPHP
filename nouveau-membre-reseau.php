f087eb1800e1a07f19ce435e506b7a15a2fd5290
ggramaize <geoffroy+dev@gramaize.eu>
2018-02-09 05:10:58 +0100

    Correction lisibilité nouveau membre réseau + mutu CSS

--- nouveau-membre-reseau.php
+++ nouveau-membre-reseau.php
@@ -42,95 +42,129 @@ if (isset($_POST['valider'])) {
  ?>
 
 <html>
-<head>
+	<head>
 		<title>Ajouter un nouveau membre réseau</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="assets/style.css">
 	</head>
-<body>
-	<form method="POST">
-		<table width="100%">
-			<tr width="100%"><td colspan="2" style="background-color: orange; color: blue;">
-<div align="center"><span style="background: orange; font-size : 18px; color: blue;"><b>Ajouter un nouveau membre réseau depuis un membre déjà présent</b></span></br></div>
-		</td></tr>
+	<body>
 		<form method="POST">
-			<tr width="100%"><td width="30%">
-				Sélection du membre :</td><td width="70%">
-      	<select name="membre">
-		<?php $sql = "SELECT id, indicatif_membre, nom_membre, prenom_membre FROM membres";
+			<table class="formfield">
+				<tr>
+					<td colspan="2" class="titre">Ajouter un nouveau membre réseau depuis un membre déjà présent</td>
+				</tr>
+				<form method="POST">
+				<tr>
+					<td>Sélection du membre :</td>
+					<td>
+	      					<select name="membre">
+<?php 
+
+$sql = "SELECT id, indicatif_membre, nom_membre, prenom_membre FROM membres";
 if ($res = $bdd->query($sql)) {
 
-   /* Récupère le nombre de lignes qui correspond à la requête SELECT */
-   if ($res->fetchColumn() > 0) {
+	/* Récupère le nombre de lignes qui correspond à la requête SELECT */
+	if ($res->fetchColumn() > 0) {
 
-      /* Effectue la vraie requête SELECT et travaille sur le résultat */
-      $sql = "SELECT id, indicatif_membre, nom_membre, prenom_membre FROM membres";
-      foreach ($bdd->query($sql) as $row) {
+		/* Effectue la vraie requête SELECT et travaille sur le résultat */
+		$sql = "SELECT id, indicatif_membre, nom_membre, prenom_membre FROM membres";
+		foreach ($bdd->query($sql) as $row) {
+?>
+						<option value="<?php echo( $row['indicatif_membre']); ?>"><?php echo( strtoupper($row['indicatif_membre']).', '.ucfirst($row['prenom_membre']).' '.strtoupper($row['nom_membre'])); ?></option>
+';
+<?php
+		}
+	}
+} ?>
+						</select>
+					</td>
+				</tr>
+	
+				<tr>
+					<td>Alias tactique :</td>
+					<td>
+						<input type="text" name="alias_tactique" id="alias_tactique" placeholder ="Alias tactique">
+					</td>
+				</tr>
+	
+				<tr>
+					<td>Etat :</td>
+					<td>
+						<select name="etat">
+							<option value="actif"  style="background-color: green;">Actif</option>
+							<option value="inactif" style="background-color: orangered;">Inactif</option>
+							<option value="repos" style="background-color: dodgerblue;">Repos</option>
+							<option value="indisponible" style="background-color: red;">Indisponible</option>
+						</select>
+					</td>
+				</tr>
+	
+				<tr>
+					<td>Commentaire :</td>
+					<td>
+						<input type="text" name="commentaire" id="commentaire" placeholder="Commentaire">
+					</td>
+				</tr>
+		
+				<tr>
+					<td class="td_submit" colspan="2">
+						<input type="submit" id="valider" name="valider" value="Valider"><br />
+						<br />
+					</td>
+				</tr>
+	
+	
+				<tr>
+					<td colspan="2" class="titre">Ajouter un nouveau membre réseau non présent dans les membres</td>
+				</tr>
+				<form method="POST">
+					<tr>
+						<td>Indicatif :</td>
+						<td>
+							<input type="text" name="indicatif_nv" id="indicatif_nv" placeholder="Indicatif">
+						</td>
+					</tr>
+				
+					<tr>
+						<td>Alias tactique :</td>
+						<td>
+							<input type="text" name="alias_tactique_nv" id="alias_tactique_nv" placeholder ="Alias tactique">
+						</td>
+					</tr>
+			
+					<tr>
+						<td>Opérateur :</td>
+						<td>
+							<input type="text" name="operateur_nv" id="operateur_nv" placeholder="Opérateur">
+						</td>
+					</tr>
+			
+					<tr>
+						<td>Etat :</td>
+						<td>
+							<select name="etat_nv">
+								<option value="actif"  style="background-color: green;">Actif</option>
+								<option value="inactif" style="background-color: orangered;">Inactif</option>
+								<option value="repos" style="background-color: dodgerblue;">Repos</option>
+								<option value="indisponible" style="background-color: red;">Indisponible</option>
+							</select>
+						</td>
+					</tr>
 
-      print '
-      	<option value="'.$row['indicatif_membre'].'">'.strtoupper($row['indicatif_membre']).', '.ucfirst($row['prenom_membre']).' '.strtoupper($row['nom_membre']).'</option>
-            ';
-      }
-   }} ?>
-   </select>
-      	</td></tr>
+					<tr>
+						<td>Commentaire :</td>
+						<td>
+							<input type="text" name="commentaire_nv" id="commentaire_nv" placeholder="Commentaire">
+						</td>
+					</tr>
 
-		<tr width="100%"><td width="30%">
-		Alias tactique :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="alias_tactique" id="alias_tactique" placeholder ="Alias tactique"></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Etat :</td><td width="70%">
-		<select name="etat">
-      	<option value="actif"  style="background-color: green;">Actif</option>
-      	<option value="inactif" style="background-color: orangered;">Inactif</option>
-      	<option value="repos" style="background-color: dodgerblue;">Repos</option>
-      	<option value="indisponible" style="background-color: red;">Indisponible</option>
-      	</select></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Commentaire :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="commentaire" id="commentaire" placeholder="Commentaire"></td></tr>
-
-		<tr width="100%"><td colspan="2">
-		<div align="center">
-			<input type="submit" style="margin-top : 10px;" id="valider" name="valider" value="Valider"></br></br></td></tr>
-
-
-	<tr width="100%"><td colspan="2" style="background-color: orange; color: blue;">
-<div align="center"><span style="background: orange; font-size : 18px; color: blue;"><b>Ajouter un nouveau membre réseau non présent dans les membres</b></span></br></div>
-		</td></tr>
-		<form method="POST">
-		<tr width="100%"><td width="30%">
-		Indicatif :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="indicatif_nv" id="indicatif_nv" placeholder="Indicatif"></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Alias tactique :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="alias_tactique_nv" id="alias_tactique_nv" placeholder ="Alias tactique"></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Opérateur :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="operateur_nv" id="operateur_nv" placeholder="Opérateur"></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Etat :</td><td width="70%">
-		<select name="etat_nv">
-      	<option value="actif"  style="background-color: green;">Actif</option>
-      	<option value="inactif" style="background-color: orangered;">Inactif</option>
-      	<option value="repos" style="background-color: dodgerblue;">Repos</option>
-      	<option value="indisponible" style="background-color: red;">Indisponible</option>
-      	</select></td></tr>
-
-		<tr width="100%"><td width="30%">
-		Commentaire :</td><td width="70%">
-		<input type="text" style="width : 200px;" name="commentaire_nv" id="commentaire_nv" placeholder="Commentaire"></td></tr>
-
-		<tr width="100%"><td colspan="2">
-		<div align="center">
-			<input type="submit" style="margin-top : 10px;" id="valider_nv" name="valider_nv" value="Valider"></td></tr></form>
-</table>
-		</div>
-	</table>
-	</form>
-</body>
+					<tr>
+						<td class="td_submit" colspan="2">
+							<input type="submit" id="valider_nv" name="valider_nv" value="Valider">
+						</td>
+					</tr>
+				</form>
+			</table>
+		</form>
+	</body>
 </html>
