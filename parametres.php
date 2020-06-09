9dab62e33f583714793ae785f1b0b13475656de6
Johan Denoyer <johan@johandenoyer.fr>
2020-06-09 19:35:34 +0200

    L'utilisation de DEFINE alors que la constante n'est pas utilisé dans le fichier en lui-même provoque un warning dans PHP7+. Passage en CONST au lieu de DEFINE résoud le pb

--- parametres.php
+++ parametres.php
@@ -1,20 +1,20 @@
 <?php 
 
 // Nom d'utilisateur du compte sur le serveur de BDD
-define( DB_USERNAME, 'root');
+const DB_USERNAME = 'gesadra';
 
 // Mot de passe du compte sur le serveur de BDD
-define( DB_PASSWORD, '');
+const DB_PASSWORD = 'gesadra';
 
 // Nom d'hote ou IP du serveur de BDD
-define( DB_HOST, 'localhost');
+const DB_HOST = 'localhost';
 
 // Nom de la BDD à utiliser
-define( DB_NAME, 'gesadra');
+const DB_NAME = 'gesadra';
 
 
 // -- NE PAS MODIFIER EN DESSOUS DE CETTE LIGNE
 // Construction du DSN pour PDO
-define( DB_PDO_DSN, 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8');
+const DB_PDO_DSN = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8';
 
 ?>
